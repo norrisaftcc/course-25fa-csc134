74a9b1041c9406c410046944921aee2ee651d01e
teacherbot <norrisa@faytechcc.edu>
2025-08-27 14:39:30 +0000

    started m1lab

---
+++ m1lab.cpp
@@ -0,0 +1,27 @@
+/*
+csc 134
+m1lab
+norrisa
+8/27/2025
+purpose: The "gem" (replace noun) sales program.
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    // start program
+    // declare all of our variables ("whiteboards")
+    string item_name = "gem"; // replace with your item
+    int    num_items = 10; 
+    double cost_per  = 0.25;
+
+    cout << "Welcome to the " << item_name << " store!" << endl;
+    // do the processing
+    double total_cost = num_items * cost_per;
+    
+
+    // end program
+    return 0;
+}
