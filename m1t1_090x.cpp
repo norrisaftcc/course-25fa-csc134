a4ee040274e52812cf21829bf724ce6a130f1b6e
teacherbot <norrisa@faytechcc.edu>
2025-08-31 17:15:23 +0000

    Finished M1T1!

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -4,5 +4,26 @@ M1T1 for the online sections
 norrisa
 8/31/25
 Program will greet the user.
+
+ERROR HANDLING WITH CODESPACES:
+IF it happens:
+"stage changes" , no, push directly
+"commit and sync", always
+"pull?" doesn't matter
 */
 
+// magic words
+#include <iostream>
+using namespace std; 
+
+int main() {
+    // input: start the program
+    // processing: none
+    // output: 0 (no erors)
+    // special FX: Says "Hello" to the user.
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1" 
+
+    return 0; // no errors
+}
