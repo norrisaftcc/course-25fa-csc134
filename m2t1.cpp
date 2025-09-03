dc8b66e3f3f7903f34d76f2b7e289d0602c11353
teacherbot <norrisa@faytechcc.edu>
2025-09-03 14:39:22 +0000

    pseudocode

--- m2t1.cpp
+++ m2t1.cpp
@@ -9,6 +9,18 @@ Revision of "sales" program from M1, now with user input.
 using namespace std;
 
 int main() {
+    // Declare our variables ("the truth")
+    string item = "apples";
+    double cost_per = 0.99;
+    int amount = 20;
+
+    // Greet the user
+    cout << "Hello! Welcome to our " << item << " store." << endl;
+    // Ask for information
+
+    // Do some processing
+
+    // Output the answer
 
     return 0;
 }
