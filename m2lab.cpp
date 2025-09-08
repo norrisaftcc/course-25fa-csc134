410dc30f59cf41b4c6d9274ce20e8534881a828b
teacherbot <norrisa@faytechcc.edu>
2025-09-08 14:35:27 +0000

    started M2lab

---
+++ m2lab.cpp
@@ -0,0 +1,28 @@
+/*
+CSC 134
+M2LAB - Case Study with Crates
+Norrisa
+9/8/25
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // set up the variables
+    double length, width, height; // size of crate
+    double cost_to_make, charge_to_customer; // pricing 
+    double profit; 
+
+    const double COST_PER_CUBIC_FOOT = 0.23; // relates to cost
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;// relates to charge
+    // Get the crate info
+
+
+    // Calculate the cost and prices
+
+    // Display the results
+
+    return 0; // no errors
+}
