69a6684d0f2c5ec75a624ed0d410acca9c34a03b
teacherbot <norrisa@faytechcc.edu>
2025-09-17 14:39:47 +0000

    m3t1 half done

--- m3t1_0001.cpp
+++ m3t1_0001.cpp
@@ -10,8 +10,30 @@ norrisa
 using namespace std;
 
 int main() {
-    
 
+    // Get info on the two rectangles
+    double length1, length2, width1, width2;
+    double area1, area2;
+    cout << "Enter the length and width of two rectangles." << endl;
+    cout << "First rectangle." << endl;
+    cout << "Length: ";
+    cin  >> length1;
+    cout << "Width: ";
+    cin  >> width1;
+    cout << endl;
+    cout << "Second rectangle." << endl;
+    cout << "Length: ";
+    cin  >> length2;
+    cout << "Width: ";
+    cin  >> width2;
+
+    // Calculate the area
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
+    // Print the output
+    cout << "First rectangle area  = " << area1 << endl;
+    cout << "Second rectangle area = " << area2 << endl;
 
 
     return 0; // no errors
