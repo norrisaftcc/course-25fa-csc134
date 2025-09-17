649288a6e532cdb4f83c1401ba9de3652808e45d
teacherbot <norrisa@faytechcc.edu>
2025-09-17 14:22:33 +0000

    started m3t1

---
+++ m3t1_0001.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M3T1 - Area of Rectangles
+Simple comparison.
+norrisa
+9/17/25
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+
+
+    return 0; // no errors
+}
