a91b9e92ecd13f3dca66362c7b92f188e8e2e581
teacherbot <norrisa@faytechcc.edu>
2025-09-22 14:57:59 +0000

    adding other options

--- m3t2_0001.cpp
+++ m3t2_0001.cpp
@@ -18,8 +18,36 @@ int roll();
 
 // main
 int main() {
-    int num = roll();
-    cout << num << endl;
+    //int num = roll();
+    //cout << num << endl;
+    // MAIN CRAPS CYCLE
+    // For now:
+    /*
+    - roll 2d6 (2-12)
+    - branch based on win, lose, or point
+    - rest comes later
+    */
+    int roll1;
+    int roll2;
+    int point;      // roll if it doesn't immediately win/lose
+
+    // Blatantly cheat
+    cout << "Enter two dice (press ENTER between) ";
+    cin >> roll1;
+    cin >> roll2;
+    int sum = roll1+roll2;
+
+    if (sum == 7) {
+        cout << "Lucky Seven -- You win!" << endl;
+    }
+    else {
+        // set the point
+        point = sum;
+        cout << "Did not roll a seven." << endl;
+        cout << "Your point is: " << point << endl;
+    }
+
+
     return 0;
 }
 
