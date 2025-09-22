7cc9ef5844bd58eedfdd57896ebb4f31ad48c7b9
teacherbot <norrisa@faytechcc.edu>
2025-09-22 14:39:47 +0000

    m3t2 base skeleton

---
+++ m3t2_0001.cpp
@@ -0,0 +1,32 @@
+/*
+M3T2 - Craps Part 1
+CSC 134
+norrisa
+9/22/25
+Beginning of the craps game.
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+
+using namespace std;
+
+// DECLARE Helper Functions 
+int roll();
+
+// main
+int main() {
+    int num = roll();
+    cout << num << endl;
+    return 0;
+}
+
+// DEFINE Helper Functions
+int roll() {
+    // rolls a six sided die
+    // TODO
+    return 6;
+
+}
