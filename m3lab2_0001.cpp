9dd4132a1d938dca5052d37cd503c54b644b5d44
teacherbot <norrisa@faytechcc.edu>
2025-09-29 14:40:03 +0000

    bug: always gives an "A"

--- m3lab2_0001.cpp
+++ m3lab2_0001.cpp
@@ -17,9 +17,27 @@ void letter_grader();
 void combat();
 
 int main() {
-    return 0;
 
+    letter_grader();
+    // combat();
+    return 0;
 }
 
 // DEFINE the other functions here
 
+void letter_grader() {
+    // input a number grade
+    // respond with a letter grade
+    double num_grade;
+    string letter_grade;
+    cout << "Enter a number grade 0-100: ";
+    cin >> num_grade;
+
+    // Create the if statements
+    letter_grade = "A";
+
+    // Output the answer
+    cout << "A number grade of " << num_grade << " is: " << letter_grade;
+    cout << endl << endl;
+
+}
