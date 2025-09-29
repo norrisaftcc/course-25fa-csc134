56f3f184238866fdffbd7034497f44abcb4402f1
teacherbot <norrisa@faytechcc.edu>
2025-09-29 14:31:07 +0000

    Started m3lab2

---
+++ m3lab2_0001.cpp
@@ -0,0 +1,25 @@
+/*
+M3LAB2 - Letter Grades (changed to Game Mechanics Test)
+CSC 134
+norrisa
+9/29/25
+*/
+
+#include <iostream>
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
+
+using namespace std;
+
+// to make things easier, we'll write the code in a function
+// DECLARE the functions here
+void letter_grader();
+void combat();
+
+int main() {
+    return 0;
+
+}
+
+// DEFINE the other functions here
+
