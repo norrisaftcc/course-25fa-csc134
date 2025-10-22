f0f8d3c944023327ebdec1d0844e6a17fe51f35b
teacherbot <norrisa@faytechcc.edu>
2025-10-22 14:59:17 +0000

    finished m5t1

--- m5t1_001.cpp
+++ m5t1_001.cpp
@@ -14,17 +14,36 @@ void say_hello(); // says hi
 
 int get_the_answer(); // provides the answer to everything
 
-double double_a_number(); // num times two
+double double_a_number(double); // num times two
 
 int main() {
     // This program does nothing useful!
     double my_num;
     int    another_num;
 
-    cout << "Welcome to the best program ever!" << endl;
+    say_hello();
     cout << "Please enter a number (with or without decimal place)." << endl;
     cin >> my_num;
-    my_num = my_num * 2;
+    //my_num = my_num * 2;
+    my_num = double_a_number(my_num); //
     cout << "Double the number is: " << my_num << endl;
-
+    cout << "But the only answer you need is: ";
+    cout << get_the_answer() << endl;
 }
+
+// Function Definitions (the whole function!) go here
+void say_hello() {
+    // says hi
+    cout << "Welcome to the best program ever!" << endl;
+}
+
+int get_the_answer() {
+    // provides the answer to everything
+    return 42;
+} 
+
+double double_a_number(double the_num) {
+    // num times two
+    double answer = the_num * 2;
+    return answer;
+} 
