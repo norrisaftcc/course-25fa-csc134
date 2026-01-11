9849bbc6978f327e4c379e2bee96a37b3b84b581
teacherbot <norrisa@faytechcc.edu>
2026-01-11 18:15:54 +0000

    Made test.cpp to show Codespace activity.

---
+++ test.cpp
@@ -0,0 +1,12 @@
+// For this video, assume you know C++
+// python equivalent is: print("Hello world")
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, GitHub!" << endl;
+    cout << "Thank you for your cooperation" << endl;
+    cout << endl; // blank line so it looks nicer
+    return 0; // no errors
+}
